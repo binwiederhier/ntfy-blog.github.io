ea5b3f196dd2519a14829eed925595424586239f
Philipp Heckel <philipp.heckel@gmail.com>
2023-12-06 23:20:37 -0500

    Color fixes

--- docs/hooks.py
+++ docs/hooks.py
@@ -3,4 +3,4 @@ import shutil
 
 def copy_fonts(config, **kwargs):
     site_dir = config['site_dir']
-    shutil.copytree('docs/static/fonts', os.path.join(site_dir, 'get'))
+    shutil.copytree('blog/static/fonts', os.path.join(site_dir, 'get'))
